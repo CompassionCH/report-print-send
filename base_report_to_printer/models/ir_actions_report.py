afb909dd191efa051742280890082d30ed098951
Emanuel Cino <ecino@compassion.ch>
2025-06-05 15:01:14 +0200

    Revert "Prioritize User Preferences Over Report Settings for Printing"
    This reverts commit 5803e3fc77c77fe3f148ca84784afc18ecc5f4a9.

--- base_report_to_printer/models/ir_actions_report.py
+++ base_report_to_printer/models/ir_actions_report.py
@@ -97,8 +97,8 @@ class IrActionsReport(models.Model):
         self.ensure_one()
         printing_act_obj = self.env["printing.report.xml.action"]
 
-        result = self._get_report_default_print_behaviour()
-        result.update(self._get_user_default_print_behaviour())
+        result = self._get_user_default_print_behaviour()
+        result.update(self._get_report_default_print_behaviour())
 
         # Retrieve report-user/language specific values
         print_action = printing_act_obj.search(
